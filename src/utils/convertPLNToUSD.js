87ce24d62e28df2fda7143e2d4d70fbeca8970d9
Sylwia Ziemińska <sylwia.zieminskaa@gmail.com>
2022-02-06 20:03:48 +0100

    initial commit

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,18 @@
+export const convertPLNToUSD = (PLN) => {
+  
+  if(typeof(PLN) === 'string' || PLN === undefined) {
+    return NaN
+  } else if(typeof(PLN) !== 'string' && typeof(PLN) !== 'number') {
+    return 'Error'
+  } else if (PLN <= 0) {
+    PLN = 0
+  }
+
+  const PLNtoUSD = PLN / 3.5;
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  return formatter.format(PLNtoUSD);
+}
